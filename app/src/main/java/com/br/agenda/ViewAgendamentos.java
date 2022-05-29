e7c2d043ee3ab788cb3455d5cb94743d6a590926
Gustavo Macedo <gmnascimento.95@outlook.com.br>
2022-05-29 04:07:44 -0300

    update projeto

--- app/src/main/java/com/br/agenda/ViewAgendamentos.java
+++ app/src/main/java/com/br/agenda/ViewAgendamentos.java
@@ -2,19 +2,23 @@ package com.br.agenda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.br.agenda.controller.AgendamentoController;
 import com.br.agenda.model.bean.Agendamento;
 import com.br.agenda.model.bean.Usuario;
-import com.br.agenda.model.dao.AgendamentoDAO;
 
-import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 public class ViewAgendamentos extends AppCompatActivity {
     private Usuario usuario;
+    private TextView txtConteudo;
+    private TextView txtHora;
+    private TextView txtData;
+    private TextView txtDescricao;
+    private TextView txtContato;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +33,22 @@ public class ViewAgendamentos extends AppCompatActivity {
 
             List<Agendamento> agendamentoList = agendamentoController.listarAgendamentos(agendamento);
 
+            txtConteudo = (TextView) findViewById(R.id.textDescricao);
+            txtData = (TextView) findViewById(R.id.textData);
+            txtHora = (TextView) findViewById(R.id.textHora);
+            txtDescricao = (TextView) findViewById(R.id.textConteudo);
+            txtContato = (TextView) findViewById(R.id.textContato);
+
+            SimpleDateFormat hora = new SimpleDateFormat("HH:mm");
+            SimpleDateFormat data = new SimpleDateFormat("dd-mm-yyyy");
+            System.out.println(agendamentoList.get(0));
             if(agendamentoList != null){
                 for (Agendamento age : agendamentoList){
-                    System.out.println(age);
+                    txtConteudo.setText(age.getConteudo());
+                    txtHora.setText(hora.format(age.getHora_agendada()));
+                    txtData.setText(data.format(age.getData_agendada()));
+                    txtDescricao.setText(age.getDescricao());
+                    txtContato.setText(age.getContato().getNome());
                 }
             }
 
